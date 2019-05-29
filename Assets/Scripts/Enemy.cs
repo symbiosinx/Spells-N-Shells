67a1b6793e8e18cfb9e84f522e8890920503e3d8
MyopicMuppet <47373110+MyopicMuppet@users.noreply.github.com>
2019-05-29 15:33:58 +1000

    enemy placeholder update

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour {
-
+    public Transform target;
 	void Shoot() {
 	}
 }
